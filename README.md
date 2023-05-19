275acb4a73d6b4fc2b543e73a4a41b7be5ca255f
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-05-19 19:52:29 +0530

    CSS Rem units and BEM Method

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 - The best way to perform a basic reset using the universal selector.
 - How to set project wide font definitions.
 - How to clip parts of elements using clip-path.
+
+## Part - 02
+
+- Great workflow to convert px into rem
+- How to BEN method in Practice
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,16 +5,16 @@ import logo from "../assets/logo-white.png";
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={logo} alt="logo" className="logo" />
+      <div className="header__logo-box">
+        <img src={logo} alt="logo" className="header__logo" />
       </div>
-      <div className="main-content">
+      <div className="header__main-content">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happening</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happening</span>
         </h1>
 
-        <a href="#" className="btn btn-white btn-animated">
+        <a href="#" className="btn btn--white btn--animated">
           Discover our tours
         </a>
       </div>
